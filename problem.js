529ba12500454baf5b44f3bbd96281ec0b68934c
Arpit Purwar <arpurwar@paypal.com>
2022-09-17 13:04:03 +0530

    problem

--- problem.js
+++ problem.js
@@ -1,21 +1,22 @@
-const arr = [2,5,5,9,11,12,12];
-const target = 12;
+const arr = [5,7,7,8,8,10]
+const target = 7;
 
-function right(arr, target){
+function searchRight(arr, target){
 	let left = -1;
-	let right = arr.length;
+	let right = arr.length; 
 
 	while(left+1 < right){
 		let mid = Math.floor((left+right)/2);
-
+		console.log('left', left, 'right', right, 'mid', mid)
 		if(arr[mid] > target){
-			right = mid;
+			right = mid
 		}else{
-			left = mid;
+			left = mid
 		}
 	}
-	console.log('left', left, 'right',right)
-	if(arr[right-1] == target){
+
+	console.log('left', left, 'right', right)
+	if(arr[right -1] === target){
 		return right-1;
 	}else{
 		return -1;
@@ -24,26 +25,41 @@ function right(arr, target){
 
 }
 
-function left(arr, target){
+function searchLeft(arr, target){
 	let left = -1;
-	let right = arr.length;
+	let right = arr.length; 
 
 	while(left+1 < right){
-		let mid = Math.floor((left + right)/2);
-		console.log(mid,left,right)
+		let mid = Math.floor((left+right)/2);
+		console.log('left', left, 'right', right, 'mid', mid)
 		if(arr[mid] >= target){
-			right = mid;
-		}
-		else{
+			right = mid
+		}else{
 			left = mid
 		}
 	}
 
+	console.log('left', left, 'right', right)
 	if(arr[right] === target){
-		return right
-	}else{
-		return -1
+		return right;
+	}
+	else {
+		return -1;
 	}
 }
-console.log('left',left(arr, target))
-console.log('right',right(arr, target))
+
+function searchTarget(arr, target){
+	const leftIndex = searchLeft(arr, target);
+	const rightIndex = searchRight(arr, target);
+
+	if(leftIndex === -1 && rightIndex === -1){
+		return [-1,-1]
+	}
+	else{
+		return [leftIndex, rightIndex]
+	}
+
+}
+
+
+console.log(searchTarget(arr, target))
