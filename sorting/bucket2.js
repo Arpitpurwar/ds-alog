b2db1ad68876f77425883d1b8bb538497cb92121
Arpit Purwar <arpurwar@paypal.com>
2022-09-28 22:58:01 +0530

    bucket2

---
+++ sorting/bucket2.js
@@ -0,0 +1,43 @@
+const bucketSort = (givenArray) => {
+  
+   const maxElement = Math.max(...givenArray); // 41
+   const minElement = Math.min(...givenArray); // 19
+   const base = 10;
+  
+   const numberOfBucket = Math.floor(maxElement/base) - Math.floor(minElement/base) + 1; // 4
+  
+   let bucket = new Array(numberOfBucket);
+   for (let i = 0; i < numberOfBucket; i++){
+       bucket[i] = [];
+   }
+  
+   for (let i = 0; i < givenArray.length; i++) {
+       let index = Math.floor(givenArray[i] / 10) - Math.floor(minElement/base); // Starting from the minElement only not from 0.
+       bucket[index].push(givenArray[i]);
+   }
+   console.log("Before Sorting Each Bucket");
+	console.log(bucket);
+   for (let i = 0; i < numberOfBucket; i++){
+	   bucket[i].sort();
+   }
+    
+
+   console.log("After Sorting Each Bucket");
+   console.log(bucket);
+  
+   let index = 0;
+   for (let i = 0; i < numberOfBucket; i++){
+		for (let j = 0; j < bucket[i].length; j++){
+		 	givenArray[index++] = bucket[i][j];
+	   	}
+   }
+
+          
+}
+
+
+let givenArray = [29, 34, 19, 39, 21, 41];
+
+bucketSort(givenArray);
+
+console.log(givenArray);
