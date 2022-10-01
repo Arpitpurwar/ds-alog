a1ac45efaa48847c4b125f07683535fe4b9528bb
Arpit Purwar <arpurwar@paypal.com>
2022-10-01 15:59:05 +0530

    LInkedlist

---
+++ LinkedList/singleLL.js
@@ -0,0 +1,67 @@
+class SingleLLNode{
+	constructor(data){
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class SingleLL{
+	constructor(){
+		this.head = null;
+		this.tail = null;
+		this.length = 0
+	}
+
+	prepend(value){
+		const newNode = new SingleLLNode(value)
+
+		if(this.head === null){
+			this.head = newNode;
+			this.tail = newNode;
+			this.length++;
+			return this.printList()
+		}
+		else{
+			let temp = this.head;
+			newNode.next = temp;
+			this.head = newNode;
+			this.length++;
+			return this.printList();
+		}
+
+	}
+
+	append(value){
+		let newNode = new SingleLLNode(value);
+		let temp = this.tail;
+
+		temp.next = newNode;
+		this.tail = newNode;
+		this.length++;
+		return this.printList()
+	}
+
+	insertAt(position, value){
+		if(position === 0){
+			return this.prepend(value)
+		}
+		if(position === this.length-1){
+			return this.append(value);
+		}
+	}
+
+	printList(){
+		let temp = this.head;
+		while(temp){
+			console.log("-->",temp.data,"-->");
+			temp = temp.next;
+		}
+	}
+}
+
+const ll = new SingleLL()
+ll.prepend(4);
+ll.prepend(5);
+ll.append(6)
+console.log("LL", ll)
+
