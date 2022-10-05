687e3bb6828783fcf1c7e7bc83c9f18b3935c338
Arpit Purwar <arpurwar@paypal.com>
2022-10-05 13:22:45 +0530

    count occurence

---
+++ problems/sliding/count_occurence_anagram.js
@@ -0,0 +1,45 @@
+// str1 = aabaabaa
+// str2 = aaba
+// find out count of anagram of string2 which present in string 1;
+
+let str1 = 'aabaabaa';
+let str2 = 'aabaa';
+
+let map = new Map();
+for(let letter of str2){
+	if(map.has(letter)){
+		map.set(letter,map.get(letter)+1);
+	}else{
+		map.set(letter,1);
+	}	
+}
+
+let distinctValues = map.size; // distinct values in str2
+let i =0, j=0;
+let ans = 0;
+while(j < str1.length){
+	if(map.has(str1[j])){
+		let temp = map.get(str1[j])-1
+		map.set(str1[j], temp);
+		if(temp === 0){
+			distinctValues--;
+		}
+	}
+	if(j-i+1 === str2.length){
+		if(distinctValues === 0){
+			ans++;
+		}
+		if(map.has(str1[i])){
+			let temp = map.get(str1[i])+1;
+			map.set(str1[i], temp)
+
+			if(temp ===1){
+				distinctValues++;
+			}
+		}
+		i++;
+	}
+	j++;
+}
+
+console.log(ans)
